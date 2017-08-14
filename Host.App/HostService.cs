ffaa74bd77664ca063a6b6e608ed413fb51fee45
Joe Buschmann <joseph.buschmann@outlook.com>
2017-08-14 08:29:15 -0500

    Remove Async Void
    Removed async void from HttpService. Exceptions were causing the process
    to crash.

--- Host.App/HostService.cs
+++ Host.App/HostService.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Runtime.ExceptionServices;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
--- Plugin.HttpServer/HttpService.cs
+++ Plugin.HttpServer/HttpService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 using Host.Contract;
 
 namespace Plugin.HttpServer
@@ -10,7 +11,19 @@ namespace Plugin.HttpServer
         private HttpListener _httpListener;
         private int _hitCount = 0;
 
-        public override async void OnStart(string[] args)
+        public override void OnStart(string[] args)
+        {
+            StartHttpListener();
+            WaitForRequests().ContinueWith(t =>
+            {
+                if (t.IsFaulted)
+                {
+                    throw t.Exception.InnerException;
+                }
+            });
+        }
+
+        private void StartHttpListener()
         {
             _httpListener = new HttpListener();
 
@@ -22,11 +35,14 @@ namespace Plugin.HttpServer
             }
 
             _httpListener.Start();
+        }
 
+        private async Task WaitForRequests()
+        {
             while (_httpListener.IsListening)
             {
                 HttpListenerContext context = await _httpListener.GetContextAsync();
-
+                
                 _hitCount++;
                 string responseBody = $"<html><body>Hello from Http Server. You are visitor <em>#{_hitCount}</em>.</body></html>";
                 byte[] responseBytes = Encoding.UTF8.GetBytes(responseBody);
